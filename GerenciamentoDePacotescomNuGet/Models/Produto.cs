7ed5fb8e90803d3c8c40c4a6e6079572c1a6a9de
marlonballa <marlonballa07@gmail.com>
2022-12-05 09:52:16 -0300

    Fim da aula sobre: Gerenciamento de projetos com NuGet

---
+++ GerenciamentoDePacotescomNuGet/Models/Produto.cs
@@ -0,0 +1,9 @@
+namespace GerenciamentoDePacotescomNuGet
+{
+    public class Produto
+    {
+        public string NomeProduto { get; set; } = string.Empty;
+        public double ValorProduto { get; set; }
+        public double QuantidadeProduto { get; set; }
+    }
+}
---
+++ GerenciamentoDePacotescomNuGet/Program.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using GerenciamentoDePacotescomNuGet;
+
+var primeiroProduto = new Produto
+{
+    NomeProduto = "Notebook",
+    QuantidadeProduto = 10,
+    ValorProduto = 3000
+};
+
+//Vamos transformar essa instância da classe produto em uma string no formato JSON
+var resultJson = JsonConvert.SerializeObject(primeiroProduto); 
+
+Console.WriteLine(resultJson);
+
+/*
+    - Adicionar pacotes:
+        dotnet add package nameOfPackage
+
+    - Remover pacotes:
+        dotnet remove package nameOfPackage
+*/
