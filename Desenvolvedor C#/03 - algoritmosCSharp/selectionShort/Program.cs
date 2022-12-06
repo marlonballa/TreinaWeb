572500d75b0fd4bf8cd1f388c612d2701879aec0
marlonballa <marlonballa07@gmail.com>
2022-12-06 12:10:34 -0300

    Correção do algoritmo de ordenação: Selection Sort

--- Desenvolvedor C#/03 - algoritmosCSharp/selectionShort/Program.cs
+++ Desenvolvedor C#/03 - algoritmosCSharp/selectionShort/Program.cs
@@ -7,7 +7,7 @@ for (int contExterno = 0; contExterno < selectionSort.Length; contExterno++)
     //Para que seja possível fazer isso, logo na primeira iteração, devemos informar a variável menor o indice do array.
     menor = contExterno;
 
-    for (int contInterno = ++contExterno; contInterno < selectionSort.Length; contInterno++)
+    for (int contInterno = contExterno + 1; contInterno < selectionSort.Length; contInterno++)
     {
         //Estamos comparando se o valor contido no laço interno é menor do que o que está contido no laço externo
         //Como assumimos que o primeiro valor é o menor, caso nesta comparação ele seja maior do que o valor seguinte, devemos informar o indice do menor valor para a variável "menor"
@@ -20,7 +20,7 @@ for (int contExterno = 0; contExterno < selectionSort.Length; contExterno++)
         //Verifica se houve alteração no valor da variável menor. 
         //Caso tenha acontecido, realiza a troca de posição dentro do array, colocando o menor valor a esquerda e o maior valor no lugar do outro. 
 
-        if (menor != selectionSort[contExterno])
+        if (menor != contExterno)
         {
             var armazenaContExterno = selectionSort[menor];
             selectionSort[menor] = selectionSort[contExterno];
