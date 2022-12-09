0d2666928f427d70b378581734b6967e47eabad5
marlonballa <marlonballa07@gmail.com>
2022-12-09 20:25:36 -0300

    TreinaWeb - ASP.NET Core - Fundamentos: O que é uma Template Engine

---
+++ Desenvolvedor ASP.NET Core/01 - Fundamentos/toDoListTreinaWeb/Program.cs
@@ -0,0 +1,26 @@
+using Controllers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+//Define qual página será a inicial do projeto
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
